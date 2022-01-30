434df63a2443adc80902aec548a704a30a35771b
Oleksii Kriuchkov <lexx2109@ukr.net>
2022-01-30 20:16:35 +0200

    same

--- client/src/Components/SearchTickers.jsx
+++ client/src/Components/SearchTickers.jsx
@@ -6,12 +6,19 @@ const SearchTickers = () => {
 
   return <div>
     <Form className='d-flex justify-content-between' style={{ width: 700, margin: 'auto', background: '#ddd' }}>
+      <label
+        style={{ width: 200 }}
+        className='d-flex align-items-center justify-content-center'
+        htmlFor="FormControl"
+      >
+        <div>Search stock or ticker</div>
+      </label>
       <FormControl
-       style={{width: "500px"}}
-        placeholder="Enter ticker's name"
+        style={{ width: "500px" }}
+        placeholder="Enter stock's name"
         onChange={e => setInputValue(e.target.value)}
       />
-      <Button style={{width: "150px"}}>Search ticker</Button>
+      {/* <Button style={{width: "150px"}}>Search ticker</Button> */}
     </Form>
   </div>;
 };
