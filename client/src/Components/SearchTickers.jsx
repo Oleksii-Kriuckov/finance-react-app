84748a4d263ebdd4e1109e49fa96ed085ab597af
Oleksii Kriuchkov <lexx2109@ukr.net>
2022-02-03 23:42:19 +0200

    safff

--- client/src/Components/SearchTickers.jsx
+++ client/src/Components/SearchTickers.jsx
@@ -1,26 +1,36 @@
 import React, { useState } from 'react';
 import { Button, Form, FormControl } from 'react-bootstrap'
+import { DropDownItem } from './DropDownItem';
 
-const SearchTickers = () => {
+const SearchTickers = ({ children, ...props }) => {
   const [inputValue, setInputValue] = useState('');
 
   return <div>
     <Form className='d-flex justify-content-between' style={{ width: 700, margin: 'auto', background: '#ddd' }}>
       <label
-        style={{ width: 200 }}
+        style={{ width: 250 }}
         className='d-flex align-items-center justify-content-center'
-        htmlFor="FormControl"
+        htmlFor="input"
       >
-        <div>Search stock or ticker</div>
+        <div>Search stock or ticker and add it</div>
       </label>
-      <FormControl
-        style={{ width: "500px" }}
-        placeholder="Enter stock's name"
-        onChange={e => setInputValue(e.target.value)}
-      />
-      {/* <Button style={{width: "150px"}}>Search ticker</Button> */}
+      <div>
+        <FormControl
+          name="input"
+          style={{ width: "var(--widthInput)" }}
+          placeholder="Enter stock's name"
+          onChange={e => setInputValue(e.target.value)}
+        />
+        <div className='dropdown'>
+        {/* {props.allStocks.map((elem, ind) => 
+          <DropDownItem key={ind}>{`"${elem.name}", ticker: ${elem.ticker}`}</DropDownItem>
+        )} */}
+        </div>
+
+      </div>
     </Form>
-  </div>;
+
+  </div >;
 };
 
 export default SearchTickers;
---
+++ server/data.js
@@ -0,0 +1,28 @@
+
+const allStocks = [
+  { name: "Advanced Micro Devices, Inc.", ticker: 'AMD', exchange: 'NASDAQ' },
+  { name: "Adobe Systems Incorporated", ticker: "ADBE", exchange: 'NASDAQ' },
+  { name: "Alphabetcd server", ticker: 'GOOGL', exchange: 'NASDAQ' },
+  { name: "Amazon.com Inc", ticker: "AMZN", exchange: 'NASDAQ' },
+  { name: "Apple", ticker: 'AAPL', exchange: 'NASDAQ' },
+  { name: "Microsoft", ticker: 'MSFT', exchange: 'NASDAQ' },
+  { name: "eBay Inc", ticker: "EBAY", exchange: 'NASDAQ' },
+  { name: "Meta Platforms, Inc.", ticker: 'FB', exchange: 'NASDAQ' },
+  { name: "Tesla", ticker: 'TSLA', exchange: 'NASDAQ' },
+  { name: "NVIDIA", ticker: 'NVDA', exchange: 'NASDAQ' },
+  { name: "Netflix Inc", ticker: 'NFLX', exchange: 'NASDAQ' },
+  { name: 'Ford Motor Co', ticker: "F", exchange: 'NYSE' },
+  { name: 'AT&T Inc', ticker: 'T', exchange: 'NYSE' },
+  { name: "Boeing", ticker: "BA", exchange: 'NYSE' },
+  { name: "Exxon Mobil Corp", ticker: "XOM", exchange: 'NYSE' },
+  { name: "The Walt Disney Company", ticker: "DIS", exchange: 'NYSE' },
+  { name: "Chevron Corp", ticker: "CVX", exchange: 'NYSE' },
+  { name: "Coca-Cola Co", ticker: "KO", exchange: 'NYSE' },
+  { name: "Oracle Corporation", ticker: "ORCL", exchange: 'NYSE' },
+  { name: "McDonald’s Corporation", ticker: "MCD", exchange: 'NYSE' },
+  { name: "Motorola Solutions Inc", ticker: "MSI", exchange: 'NYSE' },
+  { name: "General Motors Company", ticker: "GM", exchange: 'NYSE' },
+  { name: "Pfizer Inc", ticker: "PFE", exchange: 'NYSE' },
+]
+
+module.exports = allStocks
